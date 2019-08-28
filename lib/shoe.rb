de1eab7ca1da121cc2e85372f355a21d987d1d6a
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-28 09:59:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-ALL_BRANDS = []
 BRANDS = []
   def initialize(brand)
     @brand = brand
-    ALL_BRANDS << brand
-    BRANDS << ALL_BRANDS.uniq
+    BRANDS << brand
   end
 
   def cobble
